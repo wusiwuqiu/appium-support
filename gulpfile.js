04ec3c834665606668acc1c8dc8a246664763714
wusiwuqiu <wusiwuqiu@126.com>
2020-07-23 12:38:05 +0800

    来源 https://gitee.com/tong_yu_wan/appium-support.git

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const gulp = require('gulp');
+const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+
+boilerplate({
+  build: 'appium-support',
+  coverage: {
+    files: [
+      './build/test/**/*-specs.js',
+      '!./build/test/assets/**',
+      '!./build/test/**/*-e2e-specs.js'
+    ],
+    verbose: true,
+  },
+});
